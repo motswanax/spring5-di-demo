df688641640e1be75308c76f9fd3ceb5c91f9833
Baiketli Dijo <baiketlid@gmail.com>
2019-02-14 15:01:21 +0000

    Initial commit

---
+++ src/main/java/com/svs/DiSpringDemoApplication.java
@@ -0,0 +1,14 @@
+package com.svs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiSpringDemoApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/svs/controllers/MyController.java
@@ -0,0 +1,4 @@
+package com.svs.controllers;
+
+public class MyController {
+}
